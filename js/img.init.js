86467bf48343e1f47e2137b629e854bb718d97d2
韩煦 <hanxu@hanxudeMacBook-Pro.local>
2022-11-22 10:15:17 +0800

    修改图片路径

--- js/img.init.js
+++ js/img.init.js
@@ -1,8 +1,7 @@
 // 线上图片地址
 function getImgSrc(src) {
-  // var imgRoot = "/content/map/hr-chengzhitujian/";
-  // var imgRoot = currImgRoot||"./";
-  var imgRoot = "./";
+  var imgRoot = currImgRoot || "./";
+  // var imgRoot = "./";
   src = src.indexOf("/") == 0 ? src.substring(1) : src;
   return imgRoot + src;
 }
